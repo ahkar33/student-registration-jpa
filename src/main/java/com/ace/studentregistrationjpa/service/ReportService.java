ce89ae3ce695b215a431e30088355e712ecd4900
Ahkar Toe Maw <80157174+ahkar33@users.noreply.github.com>
2022-07-20 16:20:45 +0630

    added excel report

--- src/main/java/com/ace/studentregistrationjpa/service/ReportService.java
+++ src/main/java/com/ace/studentregistrationjpa/service/ReportService.java
@@ -19,6 +19,10 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleXlsxReportConfiguration;
 
 @Service
 public class ReportService {
@@ -37,13 +41,22 @@ public class ReportService {
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("createdBy", "Java Techie");
         JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSourse);
-        if(reportFormat.equalsIgnoreCase("html")) {
-            JasperExportManager.exportReportToHtmlFile(jasperPrint, path + "\\student.html");
-        }
+        // if(reportFormat.equalsIgnoreCase("html")) {
+        //     JasperExportManager.exportReportToHtmlFile(jasperPrint, path + "\\student.html");
+        // }
         if(reportFormat.equalsIgnoreCase("pdf")) {
             JasperExportManager.exportReportToPdfFile(jasperPrint, path + "\\student.pdf");
         }
-        // return "report generated in path : " + path;
+        if(reportFormat.equalsIgnoreCase("excel")){
+          JRXlsxExporter exporter = new JRXlsxExporter(); // initialize exporter 
+          exporter.setExporterInput(new SimpleExporterInput(jasperPrint)); // set compiled report as input
+          exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(path+ "\\students.xlsx"));  // set output file via path with filename
+          SimpleXlsxReportConfiguration configuration = new SimpleXlsxReportConfiguration();
+          configuration.setOnePagePerSheet(true); // setup configuration
+          configuration.setDetectCellType(true);
+          exporter.setConfiguration(configuration); // set configuration
+          exporter.exportReport();
+        }
     }
 
 }
